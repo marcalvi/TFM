0bdc395d01d5123529b5f876af83eba3c4eb1a92
Marc Albesa <albesa.marc@gmail.com>
2026-02-28 18:37:18 +0100

    first_try

--- methods/MLPs/runs/run_ZI_MIMM.sh
+++ methods/MLPs/runs/run_ZI_MIMM.sh
@@ -2,22 +2,26 @@
 set -euo pipefail
 
 # Activate conda environment
-source /opt/miniconda3/etc/profile.d/conda.sh
+#source /opt/miniconda3/etc/profile.d/conda.sh
+source /home/osiris-user/anaconda3/etc/profile.d/conda.sh
 conda activate TFM
 
 # Optional WandB login from environment variable
 wandb login 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
 
 # Define paths
-PROJECT_ROOT="/Users/marcalbesa/Desktop/TFM/git_exp/methods"
-DATA_ROOT="/Users/marcalbesa/Desktop/TFM/data/MIMM"
+#PROJECT_ROOT="/Users/marcalbesa/Desktop/TFM/git_exp/methods"
+#DATA_ROOT="/Users/marcalbesa/Desktop/TFM/data/MIMM"
+
+PROJECT_ROOT="/home/osiris-user/Desktop/TFM/methods"
+DATA_ROOT="/nfs/rnas/projects/M3BENCH/data/inputs/MIMM/"
 RESULTS_ROOT="${PROJECT_ROOT}/results"
 
 # Define endpoint
 ENDPOINT="OS_6"
 
 # Proposed tuning grid
-SEEDS="22,33"
+SEEDS="22,2002,4,80,348,7391,1414,18473,92841,55602"
 INNER_SPLITS=5
 OUTER_SPLITS=5
 EPOCHS=80
