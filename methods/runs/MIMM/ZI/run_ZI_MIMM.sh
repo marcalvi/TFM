7ef941536b74aa9ca46cb0400e49b2900a20076b
marcalvi <albesa.marc@gmail.com>
2026-04-14 14:21:35 +0200

    noveds cosetes

--- methods/runs/MIMM/ZI/run_ZI_MIMM.sh
+++ methods/runs/MIMM/ZI/run_ZI_MIMM.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Activate conda environment
 #source /opt/miniconda3/etc/profile.d/conda.sh
 source /home/osiris-user/anaconda3/etc/profile.d/conda.sh
-conda activate TFM_5090
+conda activate TFM
 
 # WandB login token
 WANDB_LOGIN_KEY="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"
@@ -23,7 +23,7 @@ fi
 # Radio aggregation method
 RADIO_AGGREGATION_METHOD="mean"
 RETRAIN_OUTER="false"
-REDUCED_DF="false"
+REDUCED_DF="true"
 
 # Define endpoint
 ENDPOINT="OS_9"
