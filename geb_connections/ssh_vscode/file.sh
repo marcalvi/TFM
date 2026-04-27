cc89143d42692c1c965693c72275d24ce0818502
marcalvi <144903514+marcalvi@users.noreply.github.com>
2026-04-27 10:41:44 +0200

    asd

--- geb_connections/ssh_vscode/file.sh
+++ geb_connections/ssh_vscode/file.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 set -euo pipefail
-
 echo "MY_VAR1: $MY_VAR1"
 echo "MY_VAR2: $MY_VAR2"
-#nvidia-smi
-. /home/osiris-user/geb-activate.sh
-conda env list
+echo "PUBLIC_KEY: $PUBLIC_KEY"
+nvidia-smi
+. /start.sh
